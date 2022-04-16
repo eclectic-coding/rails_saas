c72dc8eeb1c0392b5f2e3adfa348c4d9d124fb03
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-04-16 16:47:15 -0400

    set up members and tenant relatiosnhsips

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,4 +1,11 @@
 class Tenant < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
+  has_many :members, dependent: :destroy
+  has_many :users, through: :members
+
+  def to_s
+    name
+  end
+
 end
